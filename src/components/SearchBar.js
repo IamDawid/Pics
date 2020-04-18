400d7252004a9833421425a7f3f8d91042f4c112
Dawid <david.d.bialy@gmail.com>
2020-04-19 00:29:43 +0100

    - custom onSubmit event added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,12 +2,19 @@ import React from 'react';
 
 class SearchBar extends React.Component {
     state = { term: '' };
-   
+
+    onFormSubmit = (event) => {  //going with the function to avoid the 'undefined' error
+
+        event.preventDefault(); //disables the default submit behaviour
+
+        console.log(this.state.term);
+
+    };
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
                         <input //this is a controlled element, means all data is stored in and driven by React
